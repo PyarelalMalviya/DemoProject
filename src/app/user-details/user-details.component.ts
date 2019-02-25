dd58f41d30d8bdfea0b41e17b91d2428bd32a4c6
PyarelalMalviya <pyarelal.malwiya@gmail.com>
2019-02-25 13:21:19 +0530

    imported bootstrap

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
-
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-user-details',
